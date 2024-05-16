fba2abf6fe803aa5740fc4d4b558cc8efee1935c
Rayssa Freitas <149340917+rayssa-freitas@users.noreply.github.com>
2024-05-16 12:45:06 -0300

    models feito

---
+++ src/models/Destino.js
@@ -0,0 +1,37 @@
+const { DataTypes } = require('sequelize')
+const { connection } = require('../database/connection')
+const Usuario = require('./Usuario')
+
+const Destino = connection.define('destinos', {
+    usuario_id:{
+     type: DataTypes.INTEGER,
+    },
+    destino_nome:{
+     type: DataTypes.STRING,
+    },
+    localizacao:{
+     type: DataTypes.STRING,
+    },
+    descricao:{
+     type: DataTypes.STRING,
+    },
+    cep:{
+     type: DataTypes.INTEGER,
+    },
+    latitude:{
+     type: DataTypes.FLOAT,
+    },
+    longitude:{
+     type: DataTypes.FLOAT,
+    }
+})
+
+Destino.belongsTo(Usuario, {
+ foreignKey: 'usuario_id'
+})
+
+Usuario.hasMany(Destino, {
+ foreignKey: 'usuario_id'
+})
+
+module.exports = Destino
