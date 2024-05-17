4a2bd990e48394e7ef7769aceba201aeb4669f48
Rayssa Freitas <149340917+rayssa-freitas@users.noreply.github.com>
2024-05-17 15:49:54 -0300

    arrumando rota destinos

--- src/routes/destinos.route.js
+++ src/routes/destinos.route.js
@@ -1,35 +1,96 @@
-// const { Router, query } = require('express') 
+const { Router } = require('express') 
 
-// const DestinoController = require('../controllers/DestinoController')
+const DestinoController = require('../controllers/DestinoController')
 
-// const destinoRoutes = new Router()
+const destinoRoutes = new Router()
 
-// destinoRoutes.get('/destinos', async (req, res) => {
+destinoRoutes.get('/destinos', async (req, res) => {
 
-// })
+})
 
-// destinoRoutes.get('/destino:cep', async (req, res) => {
+destinoRoutes.get('/destino:cep', async (req, res) => {
  
-//  try{ 
+ try{ 
 
-//  }
-// })
-// destinoRoutes.post('/criarnovodestino', async (req, res) => {
+ }
+})
+destinoRoutes.post('/criarnovodestino', async (req, res) => {
+
+  try{
+   const destino_nome = req.body.destino_nome,
+   const localizacao = req.body.localizacao,
+   const descricao = req.body.descricao,
+   const cep = req.body.cep,
+   const latitude = req.body.latitude,
+   const longitude = req.body.longitude
+
+   if(!destino_nome) {
+    return res.status(400).json({
+     message: 'O preenchimento do campo destino é obrigatório!'
+    })
+   }
+
+   if(!localizacao) {
+    return res.status(400).json({
+     message: 'O preenchimento do campo localização é obrigatório!'
+    })
+   }
+
+   if(!cep) {
+    return res.status(400).json({
+     message: 'O preenchimento do campo cep é obrigatório!'
+    })
+   }
+
+   if(!latitude) {
+    return res.status(400).json({
+     message: 'O preenchimento do campo latitude é obrigatório!'
+    })
+   }
+
+   if(!longitude) {
+    return res.status(400).json({
+     message: 'O preenchimento do campo longitude é obrigatório!'
+    })
+   }
+    
+   const destino = await Destino.create({
+    destino_nome: destino_nome,
+    localizacao: localizacao,
+    descricao: descricao,
+    cep: cep,
+    latitude: latitude, 
+    longitude: longitude
+})
+res.status(201).json(usuario)
+    }catch (error) {
+      console.error(error.message)
+      res.status(500).json({
+      error: 'Não foi realizar o seu cadastro. Tente Novamente!'
+ })
+}
+}
+
+
+
+destinoRoutes.put('/atualizardestino:id', async (req, res) => {
  
-// })
-// destinoRoutes.put('/atualizardestino:id', async (req, res) => {
- 
-// })
-// destinoRoutes.delete('/atualizardestino:id', async (req, res) => {
-//  const { destino_id } = req.params
+})
 
-//  DestinoController.destroy({
-//   where: {
-//    destino_id: destino_id
-//   }
-//  })
 
-//  res.status(204).json({})
-// })
 
-// module.exports = destinoRoutes 
+
+
+destinoRoutes.delete('/atualizardestino:id', async (req, res) => {
+ const { destino_id } = req.params
+
+ DestinoController.destroy({
+  where: {
+   destino_id: destino_id
+  }
+ })
+
+ res.status(204).json({})
+})
+
+module.exports = destinoRoutes
