246d9a162301f8298a06007e98aa68e7f6e1eccf
Rayssa Freitas <149340917+rayssa-freitas@users.noreply.github.com>
2024-05-17 07:24:41 -0300

    Update Usuario.js

--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -1,4 +1,4 @@
-const { DataTypes } = require('sequelize'
+const { DataTypes } = require('sequelize')
 const { connection } = require('../database/connection') 
 
 const Usuario = connection.define('usuarios', {
